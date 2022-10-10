689a6ce4d322208d0731c9bc6d94cdd13e0666a3
Sam Lewis <sam.lewis0602@gmail.com>
2022-10-10 00:21:14 -0400

    add TODOs

---
+++ internal/http/http.go
@@ -0,0 +1,6 @@
+// http is used to interact with the home assistant
+// REST API, currently only for retrieving state for
+// a single entity_id
+package http
+
+// TODO: impl http struct, should be initialized as part of App()
---
+++ internal/websocket/websocket.go
@@ -0,0 +1,7 @@
+// websocket is used to interact with the Home Assistant
+// websocket API. All HA interaction is done via websocket
+// except for cases explicitly called out in http package
+// documentation.
+package websocket
+
+// TODO: move existing websocket code here
