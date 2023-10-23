049b0e0011b7b66fea75cd45512d234cbf411f2b
Sam Lewis <sam.lewis0602@gmail.com>
2023-10-22 23:13:47 -0400

    add some unit tests for some of the checkers

---
+++ checkers_test.go
@@ -0,0 +1,133 @@
+package gomeassistant
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"saml.dev/gome-assistant/internal"
+)
+
+type MockState struct {
+	EqualsReturn bool
+	EqualsError  bool
+	GetReturn    EntityState
+	GetError     bool
+}
+
+func (s MockState) AfterSunrise(_ ...DurationString) bool {
+	return true
+}
+func (s MockState) BeforeSunrise(_ ...DurationString) bool {
+	return true
+}
+func (s MockState) AfterSunset(_ ...DurationString) bool {
+	return true
+}
+func (s MockState) BeforeSunset(_ ...DurationString) bool {
+	return true
+}
+func (s MockState) Get(eid string) (EntityState, error) {
+	if s.GetError {
+		return EntityState{}, errors.New("some error")
+	}
+	return s.GetReturn, nil
+}
+func (s MockState) Equals(eid, state string) (bool, error) {
+	if s.EqualsError {
+		return false, errors.New("some error")
+	}
+	return s.EqualsReturn, nil
+}
+
+var runOnError = internal.EnabledDisabledInfo{
+	Entity:     "eid",
+	State:      "state",
+	RunOnError: true,
+}
+
+var dontRunOnError = internal.EnabledDisabledInfo{
+	Entity:     "eid",
+	State:      "state",
+	RunOnError: false,
+}
+
+func list(infos ...internal.EnabledDisabledInfo) []internal.EnabledDisabledInfo {
+	ret := []internal.EnabledDisabledInfo{}
+	ret = append(ret, infos...)
+	return ret
+}
+
+func TestEnabledEntity_StateEqual_Passes(t *testing.T) {
+	state := MockState{
+		EqualsReturn: true,
+	}
+	c := checkEnabledEntity(state, list(runOnError))
+	assert.False(t, c.fail, "should pass")
+}
+
+func TestEnabledEntity_StateNotEqual_Fails(t *testing.T) {
+	state := MockState{
+		EqualsReturn: false,
+	}
+	c := checkEnabledEntity(state, list(runOnError))
+	assert.True(t, c.fail, "should fail")
+}
+
+func TestEnabledEntity_NetworkError_DontRun_Fails(t *testing.T) {
+	state := MockState{
+		EqualsError: true,
+	}
+	c := checkEnabledEntity(state, list(dontRunOnError))
+	assert.True(t, c.fail, "should fail")
+}
+
+func TestEnabledEntity_NetworkError_StillRun_Passes(t *testing.T) {
+	state := MockState{
+		EqualsError: true,
+	}
+	c := checkEnabledEntity(state, list(runOnError))
+	assert.False(t, c.fail, "should fail")
+}
+
+func TestDisabledEntity_StateEqual_Fails(t *testing.T) {
+	state := MockState{
+		EqualsReturn: true,
+	}
+	c := checkDisabledEntity(state, list(runOnError))
+	assert.True(t, c.fail, "should pass")
+}
+
+func TestDisabledEntity_StateNotEqual_Passes(t *testing.T) {
+	state := MockState{
+		EqualsReturn: false,
+	}
+	c := checkDisabledEntity(state, list(runOnError))
+	assert.False(t, c.fail, "should fail")
+}
+
+func TestDisabledEntity_NetworkError_DontRun_Fails(t *testing.T) {
+	state := MockState{
+		EqualsError: true,
+	}
+	c := checkDisabledEntity(state, list(dontRunOnError))
+	assert.True(t, c.fail, "should fail")
+}
+
+func TestDisabledEntity_NetworkError_StillRun_Passes(t *testing.T) {
+	state := MockState{
+		EqualsError: true,
+	}
+	c := checkDisabledEntity(state, list(runOnError))
+	assert.False(t, c.fail, "should fail")
+}
+
+func TestStatesMatch(t *testing.T) {
+	c := checkStatesMatch("hey", "hey")
+	assert.False(t, c.fail, "should pass")
+}
+
+func TestStatesDontMatch(t *testing.T) {
+	c := checkStatesMatch("hey", "bye")
+	assert.True(t, c.fail, "should fail")
+}
--- go.mod
+++ go.mod
@@ -9,9 +9,14 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobuffalo/envy v1.10.2 // indirect
 	github.com/gobuffalo/packd v1.0.2 // indirect
 	github.com/gobuffalo/packr v1.30.1 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
