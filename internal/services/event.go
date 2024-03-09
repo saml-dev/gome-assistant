68398b02b0493624274eb26756a4da1a10f90d88
Michael Haggerty <mhagger@alum.mit.edu>
2024-03-09 17:31:25 +0100

    Add a way to fire Home Assistant events

---
+++ internal/services/event.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"context"
+
+	"saml.dev/gome-assistant/internal"
+	ws "saml.dev/gome-assistant/internal/websocket"
+)
+
+type Event struct {
+	conn *ws.WebsocketWriter
+	ctx  context.Context
+}
+
+// Fire an event
+type FireEventRequest struct {
+	Id        int64          `json:"id"`
+	Type      string         `json:"type"` // always set to "fire_event"
+	EventType string         `json:"event_type"`
+	EventData map[string]any `json:"event_data,omitempty"`
+}
+
+/* Public API */
+
+// Fire an event. Takes an event type and an optional map that is sent
+// as `event_data`.
+func (e Event) Fire(eventType string, eventData ...map[string]any) {
+	req := FireEventRequest{
+		Id:   internal.GetId(),
+		Type: "fire_event",
+	}
+
+	req.EventType = eventType
+	if len(eventData) != 0 {
+		req.EventData = eventData[0]
+	}
+
+	e.conn.WriteMessage(req, e.ctx)
+}
