06927b25ffc3ace8276f5f101585407b30332884
Michael Haggerty <mhagger@alum.mit.edu>
2024-04-20 13:27:05 +0200

    SetTemperatureRequest: distinguish between 0°C and "unset"

--- internal/services/climate.go
+++ internal/services/climate.go
@@ -33,22 +33,22 @@ func (c Climate) SetFanMode(entityID string, fanMode string) {
 }
 
 type SetTemperatureRequest struct {
-	Temperature    float32
-	TargetTempHigh float32
-	TargetTempLow  float32
+	Temperature    *float32
+	TargetTempHigh *float32
+	TargetTempLow  *float32
 	HvacMode       string
 }
 
 func (r *SetTemperatureRequest) ToJSON() map[string]any {
 	m := map[string]any{}
-	if r.Temperature != 0 {
-		m["temperature"] = r.Temperature
+	if r.Temperature != nil {
+		m["temperature"] = *r.Temperature
 	}
-	if r.TargetTempHigh != 0 {
-		m["target_temp_high"] = r.TargetTempHigh
+	if r.TargetTempHigh != nil {
+		m["target_temp_high"] = *r.TargetTempHigh
 	}
-	if r.TargetTempLow != 0 {
-		m["target_temp_low"] = r.TargetTempLow
+	if r.TargetTempLow != nil {
+		m["target_temp_low"] = *r.TargetTempLow
 	}
 	if r.HvacMode != "" {
 		m["hvac_mode"] = r.HvacMode
@@ -56,14 +56,14 @@ func (r *SetTemperatureRequest) ToJSON() map[string]any {
 	return m
 }
 
-func (c Climate) SetTemperature(entityID string, serviceData SetTemperatureRequest) {
+func (c Climate) SetTemperature(entityID string, setTemperatureRequest SetTemperatureRequest) {
 	req := CallServiceRequest{
 		Domain:  "climate",
 		Service: "set_temperature",
 		Target: Target{
 			EntityID: entityID,
 		},
-		ServiceData: serviceData.ToJSON(),
+		ServiceData: setTemperatureRequest.ToJSON(),
 	}
 
 	c.conn.Send(func(mw websocket.MessageWriter) error {
