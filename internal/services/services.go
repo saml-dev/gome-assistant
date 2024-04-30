f0699880d05136f7e757a1e79c76a133d7e023c9
Michael Haggerty <mhagger@alum.mit.edu>
2024-04-30 15:28:12 +0200

    Target: allow device targets

--- internal/services/services.go
+++ internal/services/services.go
@@ -9,11 +9,18 @@ import (
 // Target represents the target of the service call, if applicable.
 type Target struct {
 	EntityID string `json:"entity_id,omitempty"`
+	DeviceID string `json:"device_id,omitempty"`
 }
 
 func EntityTarget(entityID string) Target {
 	return Target{
 		EntityID: entityID,
+	}
+}
+
+func DeviceTarget(deviceID string) Target {
+	return Target{
+		DeviceID: deviceID,
 	}
 }
 
