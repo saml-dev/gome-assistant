6d1b0a0b2090ccad4a9581590d32ca98384a9686
Michael Haggerty <mhagger@alum.mit.edu>
2024-05-26 16:02:11 +0200

    EventContext, BaseEvent, Event, EventMessage: new types

---
+++ websocket/event_message.go
@@ -0,0 +1,24 @@
+package websocket
+
+type EventContext struct {
+	ID       string  `json:"id"`
+	UserID   *string `json:"user_id"`
+	ParentID *string `json:"parent_id"`
+}
+
+type BaseEvent struct {
+	EventType string       `json:"event_type"`
+	Origin    string       `json:"origin"`
+	TimeFired string       `json:"time_fired"`
+	Context   EventContext `json:"context"`
+}
+
+type Event struct {
+	BaseEvent
+	RawData RawMessage `json:"data"`
+}
+
+type EventMessage struct {
+	BaseMessage
+	Event Event `json:"event"`
+}
