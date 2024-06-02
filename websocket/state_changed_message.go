f278132d454a7575312ee9ead09cd09d69bb7fe8
Michael Haggerty <mhagger@alum.mit.edu>
2024-06-02 09:51:14 +0200

    EntityState: new type
    The entity state should always be a string, so add a string-based
    helper class to hold it.

--- websocket/state_changed_message.go
+++ websocket/state_changed_message.go
@@ -9,8 +9,24 @@ import (
 // "state_changed" events are compressed in a rather awkward way.
 // These types help pick them apart.
 
+// EntityState is the state of an entity ( // E.g., "on", "off",
+// "unavailable"; there are probably more.
+type EntityState string
+
+func (s EntityState) On() bool {
+	return s == "on"
+}
+
+func (s EntityState) Off() bool {
+	return s == "off"
+}
+
+func (s EntityState) Unavailable() bool {
+	return s == "unavailable"
+}
+
 type Entity struct {
-	State       RawMessage            `json:"state"`
+	State       EntityState           `json:"state"`
 	Attributes  map[string]RawMessage `json:"attributes"`
 	Context     RawMessage            `json:"context"`
 	LastChanged time.Time             `json:"last_changed"`
@@ -24,7 +40,7 @@ type EntityItem struct {
 // CompressedEntity is similar to `Entity` except that the JSON field
 // names are abbreviated.
 type CompressedEntity struct {
-	State       RawMessage            `json:"s"`
+	State       EntityState           `json:"s"`
 	Attributes  map[string]RawMessage `json:"a"`
 	Context     RawMessage            `json:"c"`
 	LastChanged time.Time             `json:"lc"`
