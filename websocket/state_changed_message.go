e027a8ec47cfd1cb20da16ed5de15d91927c5580
Michael Haggerty <mhagger@alum.mit.edu>
2024-06-02 10:20:08 +0200

    entity.go: new file, split out from `state_changed_message.go`

--- websocket/state_changed_message.go
+++ websocket/state_changed_message.go
@@ -3,48 +3,10 @@ package websocket
 import (
 	"encoding/json"
 	"log/slog"
-	"time"
 )
 
 // "state_changed" events are compressed in a rather awkward way.
 // These types help pick them apart.
-
-// EntityState is the state of an entity ( // E.g., "on", "off",
-// "unavailable"; there are probably more.
-type EntityState string
-
-func (s EntityState) On() bool {
-	return s == "on"
-}
-
-func (s EntityState) Off() bool {
-	return s == "off"
-}
-
-func (s EntityState) Unavailable() bool {
-	return s == "unavailable"
-}
-
-type Entity struct {
-	State       EntityState           `json:"state"`
-	Attributes  map[string]RawMessage `json:"attributes"`
-	Context     RawMessage            `json:"context"`
-	LastChanged time.Time             `json:"last_changed"`
-}
-
-type EntityItem struct {
-	EntityID string `json:"entity_id"`
-	Entity
-}
-
-// CompressedEntity is similar to `Entity` except that the JSON field
-// names are abbreviated.
-type CompressedEntity struct {
-	State       EntityState           `json:"s"`
-	Attributes  map[string]RawMessage `json:"a"`
-	Context     RawMessage            `json:"c"`
-	LastChanged time.Time             `json:"lc"`
-}
 
 // CompressedEntityChange keeps tracks of fields added and removed as
 // part of a change. Fields that are mutated appear as "additions".
