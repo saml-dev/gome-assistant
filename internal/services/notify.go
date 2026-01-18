943d94af71c5b568139e0438b954d9d56caaf27e
Michael Haggerty <mhagger@alum.mit.edu>
2026-01-19 00:58:16 +0100

    message.NotifyData: new type
    Replace `type.NotifyRequest` with a new `message.NotifyData` type.
    This type doesn't include the service name; instead, it should be
    passed separately to the `Notify.Notify()` method.

--- internal/services/notify.go
+++ internal/services/notify.go
@@ -4,28 +4,21 @@ import (
 	"context"
 
 	"saml.dev/gome-assistant/message"
-	"saml.dev/gome-assistant/types"
 )
 
 type Notify struct {
 	api API
 }
 
-// Notify sends a notification. Takes a types.NotifyRequest.
+// Notify sends a notification.
 func (ha *Notify) Notify(
-	ctx context.Context, reqData types.NotifyRequest,
+	ctx context.Context, serviceName string, reqData message.NotifyData,
 ) (any, error) {
 	req := message.CallServiceData{
-		Domain:  "notify",
-		Service: reqData.ServiceName,
+		Domain:      "notify",
+		Service:     serviceName,
+		ServiceData: reqData,
 	}
-	serviceData := map[string]any{}
-	serviceData["message"] = reqData.Message
-	serviceData["title"] = reqData.Title
-	if reqData.Data != nil {
-		serviceData["data"] = reqData.Data
-	}
-	req.ServiceData = serviceData
 
 	var result any
 	if err := ha.api.Call(ctx, req, &result); err != nil {
